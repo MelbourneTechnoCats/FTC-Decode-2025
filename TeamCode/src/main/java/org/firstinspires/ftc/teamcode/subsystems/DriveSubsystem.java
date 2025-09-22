43de7896a5b158b5ddd2033f077d04e93ba2775a
Vinh Nguyen <ngtv0404@gmail.com>
2025-09-22 19:07:05 +1000

    add comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -22,18 +22,21 @@ public class DriveSubsystem extends SubsystemBase {
 
     public DriveSubsystem(final HardwareMap hardwareMap, final Telemetry telemetry) {
         m_telemetry = telemetry;
+
+        /* instantiate Mecanum drivetrain object from FTCLib */
         m_drive = new MecanumDrive(
                 new Motor(hardwareMap, "frontLeftDrive", Motor.GoBILDA.RPM_312),
                 new Motor(hardwareMap, "frontRightDrive", Motor.GoBILDA.RPM_312),
                 new Motor(hardwareMap, "backLeftDrive", Motor.GoBILDA.RPM_312),
                 new Motor(hardwareMap, "backRightDrive", Motor.GoBILDA.RPM_312)
-        );
+        ); // NOTE: in this exact order
 
-        m_imu = hardwareMap.get(IMU.class, "imu");
+        /* initialise the IMU */
+        m_imu = hardwareMap.get(IMU.class, "imu"); // get IMU from robot config
         RevHubOrientationOnRobot imuOrientation = new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
-        );
+        ); // REV Control Hub orientation on robot
         m_imu.initialize(new IMU.Parameters(imuOrientation));
     }
 
@@ -49,6 +52,7 @@ public class DriveSubsystem extends SubsystemBase {
             m_drive.driveRobotCentric(m_xSpeed, m_ySpeed, m_rotSpeed);
     }
 
+    /* set velocity to run the drivetrain at */
     public void drive(double xSpeed, double ySpeed, double rotSpeed, boolean fieldCentric) {
         m_xSpeed = xSpeed;
         m_ySpeed = ySpeed;
@@ -56,6 +60,7 @@ public class DriveSubsystem extends SubsystemBase {
         m_fieldCentric = fieldCentric;
     }
 
+    /* get the drivetrain's heading */
     public Rotation2d getHeading() {
         return new Rotation2d(m_imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
     }
