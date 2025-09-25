1c431d1513755401dc409bbd0033914a977058bc
waitwut8 <135427956+waitwut8@users.noreply.github.com>
2025-09-25 18:15:35 +1000

    add squaring of inputs on DriveOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
@@ -13,31 +11,37 @@ import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 public class DriveOpMode extends CommandOpMode {
     private GamepadEx m_driveGamepad;
     private DriveSubsystem m_driveSubsystem;
+    public static double squareInput(double input){
 
-    private boolean m_fieldCentric = false;
-
+//        if (input >= 0) {
+//            output = input*input;
+//        }
+//        else{
+//            output = -input*input;
+//        }
+//        double output = input*input;
+//        if (input < 0) output *=-1;
+//        return output;
+        return Math.copySign(input*input, input);
+    }
     @Override
     public void initialize() {
         m_driveGamepad = new GamepadEx(gamepad1);
         m_driveSubsystem = new DriveSubsystem(hardwareMap, telemetry);
         m_driveSubsystem.setDefaultCommand(new RunCommand(
                 () -> {
-                    telemetry.addData("Field-centric drive", m_fieldCentric);
-//                    telemetry.update(); // NOTE: telemetry.update() seems to clear telemetry data
+                    double leftX = m_driveGamepad.getLeftX();
+                    double leftY = m_driveGamepad.getLeftY();
+                    double rightX = m_driveGamepad.getRightX();
+
 
                     m_driveSubsystem.drive(
-                            m_driveGamepad.getLeftX(),
-                            m_driveGamepad.getLeftY(),
-                            -m_driveGamepad.getRightX(),
-                            m_fieldCentric
+                            squareInput(leftX),
+                            squareInput(leftY),
+                            -squareInput(rightX),
+                            false
                     );
                 }, m_driveSubsystem
         ));
-        m_driveGamepad.getGamepadButton(GamepadKeys.Button.LEFT_STICK_BUTTON)
-                .whenPressed(new InstantCommand(
-                        () -> {
-                            m_fieldCentric = !m_fieldCentric;
-                        }
-                ));
     }
 }
