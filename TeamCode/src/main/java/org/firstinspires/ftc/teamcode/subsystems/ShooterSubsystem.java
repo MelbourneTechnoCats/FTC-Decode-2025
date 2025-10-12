5acd278f978f2ad044fe27f0159c5f852cc2b28b
waitwut8 <135427956+waitwut8@users.noreply.github.com>
2025-10-12 11:33:42 +1100

    Added more properties to the ShooterSubsystem, and added functions to return RPM required.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
@@ -10,6 +10,8 @@ import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 public class ShooterSubsystem extends SubsystemBase {
 //    MotorGroup m_motorGroup;
@@ -22,6 +24,25 @@ public class ShooterSubsystem extends SubsystemBase {
     private Telemetry m_telemetry;
     static final double MIN_ANGLE = 0;
     static final double MAX_ANGLE = 300;
+
+
+    private static final double kTagY = 61.6/100; /** in m **/
+
+    private static final Position kCameraPosition = VisionSubsystem.kCameraPosition.toUnit(DistanceUnit.METER);
+    private static final double kCameraX = kCameraPosition.y;
+    private static final double kCameraY = kCameraPosition.z;
+
+    private static final double kShooterWheelDiameter = 7.2/100; /** in m **/
+    /** constant names are in 2D, but position is in 3D **/
+    /** if we change camera position, change the variables as well **/
+    /// ////////////
+    private static final double k_xOffset = 0;
+    private static final double k_yOffset = 0;
+    /** todo: get offset done **/
+    private static final double kShooterAngle = Math.toRadians(60); /** in rad **/
+    private static final double kShooterHeight = 30.48/100; /** in m **/
+    private static final double kGravity = 9.8; /** in m/s^2 **/
+
     public ShooterSubsystem(final HardwareMap hardwareMap, Telemetry telemetry){
          m_servo = new SimpleServo(hardwareMap, "shooterServo", MIN_ANGLE, MAX_ANGLE);
          m_leftMotor = new MotorEx(hardwareMap, "leftShooterMotor");
@@ -29,6 +50,24 @@ public class ShooterSubsystem extends SubsystemBase {
          m_leftMotor.setInverted(true);
          m_motorGroup = new MotorGroup(m_leftMotor, m_rightMotor);
          m_telemetry = telemetry;
+
+
+
+
+
+
+
+    }
+
+    public double getGoalVelocity(double range){
+        double tagX = kCameraX + Math.sqrt(range*range - Math.pow((kTagY - kCameraY), 2));
+        double targetX = tagX + k_xOffset;
+        double targetY = kTagY + k_yOffset;
+        double shooterTime = Math.sqrt(2*((targetX*kShooterAngle)-targetY+kShooterHeight)/kGravity);
+        double shooterVelocity = targetX*shooterTime/Math.cos(kShooterAngle);
+        double shooterRPM = 60*shooterVelocity/(2*Math.PI*kShooterWheelDiameter/2);
+        return shooterRPM;
+
 
 
     }
