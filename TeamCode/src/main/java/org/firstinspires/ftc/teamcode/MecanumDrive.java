5f609251bfff27eb1bcb424444092d09ba84afaa
Vinh Nguyen <ngtv0404@gmail.com>
2025-10-26 11:11:49 +1100

    tune path controller gains

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -92,9 +92,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 2.5; // TODO
-        public double lateralGain = 0.0; // TODO
-        public double headingGain = 2.5; // shared with turn
+        public double axialGain = 5.0;
+        public double lateralGain = 2.5;
+        public double headingGain = 2.6; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
