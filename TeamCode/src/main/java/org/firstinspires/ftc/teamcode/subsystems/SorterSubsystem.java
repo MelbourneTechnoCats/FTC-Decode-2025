b0e82e281e9710bf50c8fb85de767ff2a96dfc00
Vinh Nguyen <ngtv0404@gmail.com>
2025-11-01 15:50:46 +1100

    implement colour sensor reading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.graphics.Color;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 public class SorterSubsystem extends SubsystemBase {
     HardwareMap m_hardwareMap;
@@ -25,9 +29,12 @@ public class SorterSubsystem extends SubsystemBase {
     static final double LEVER_RETRACT_ANGLE = 25;
     static final double LEVER_EXTEND_ANGLE = LEVER_RETRACT_ANGLE + 40;
 
+    private NormalizedColorSensor m_colourSensor;
+
     public SorterSubsystem(HardwareMap hardwareMap) {
         m_leverServo = new SimpleServo(hardwareMap, "leverServo", MIN_ANGLE, MAX_ANGLE);
         m_sorterServo = new SimpleServo(hardwareMap, "sorterServo", MIN_ANGLE, MAX_ANGLE);
+        m_colourSensor = hardwareMap.get(NormalizedColorSensor.class, "sorterColour");
         m_hardwareMap = hardwareMap;
     }
 
@@ -59,5 +66,31 @@ public class SorterSubsystem extends SubsystemBase {
     public void setLeverAngle(boolean retract)
     {
         m_leverServo.turnToAngle((retract ? LEVER_RETRACT_ANGLE : LEVER_EXTEND_ANGLE));
+    }
+
+    public enum Colour {
+        NONE,
+        PURPLE,
+        GREEN
+    }
+
+    private static final double kAlphaThreshold = 0.4;
+
+    private static final double kMinGreenHue = 90;
+    private static final double kMaxGreenHue = 180;
+
+    private static final double kMinPurpleHue = 200;
+    private static final double kMaxPurpleHue = 330;
+
+    public Colour getColour() {
+        NormalizedRGBA colours = m_colourSensor.getNormalizedColors();
+        if (colours.alpha < kAlphaThreshold) return Colour.NONE;
+
+        float[] hsvColour = new float[3];
+        Color.colorToHSV(colours.toColor(), hsvColour);
+        float hue = hsvColour[0];
+        if (hue >= kMinGreenHue && hue <= kMaxGreenHue) return Colour.GREEN;
+        else if (hue >= kMinPurpleHue && hue <= kMaxPurpleHue) return Colour.PURPLE;
+        else return Colour.NONE;
     }
 }
