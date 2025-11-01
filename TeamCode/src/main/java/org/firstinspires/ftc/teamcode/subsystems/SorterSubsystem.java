b7747b5425d207edbe54e5f7b0810491e65ca7cb
Vinh Nguyen <ngtv0404@gmail.com>
2025-11-01 16:55:23 +1100

    fix spinner angles according to final design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
@@ -17,15 +17,15 @@ public class SorterSubsystem extends SubsystemBase {
 //    static final double C2_LEVER_ANGLE = 180;
 //    static final double C0_INTAKE_ANGLE = C2_LEVER_ANGLE + 60;
 //    static final double C1_INTAKE_ANGLE = C2_LEVER_ANGLE - 60;
-//    static final double C2_INTAKE_ANGLE = C2_LEVER_ANGLE - 180;
+//    static final double C2_LEVER_ANGLE = C2_LEVER_ANGLE - 180;
 //    static final double C0_LEVER_ANGLE = C2_LEVER_ANGLE - 120;
 //    static final double C1_LEVER_ANGLE = C2_LEVER_ANGLE + 120;
-    static final double C2_INTAKE_ANGLE = 180;
-    static final double C0_INTAKE_ANGLE = C2_INTAKE_ANGLE + 60;
-    static final double C1_INTAKE_ANGLE = C2_INTAKE_ANGLE - 60;
-    static final double C2_LEVER_ANGLE = C2_INTAKE_ANGLE - 180;
-    static final double C0_LEVER_ANGLE = C2_INTAKE_ANGLE - 120;
-    static final double C1_LEVER_ANGLE = C2_INTAKE_ANGLE + 120;
+    static final double C2_LEVER_ANGLE = 180;
+    static final double C0_INTAKE_ANGLE = C2_LEVER_ANGLE + 60;
+    static final double C1_INTAKE_ANGLE = C2_LEVER_ANGLE - 60;
+    static final double C2_INTAKE_ANGLE = C2_LEVER_ANGLE - 180;
+    static final double C0_LEVER_ANGLE = C2_LEVER_ANGLE - 120;
+    static final double C1_LEVER_ANGLE = C2_LEVER_ANGLE + 120;
     static final double LEVER_RETRACT_ANGLE = 25;
     static final double LEVER_EXTEND_ANGLE = LEVER_RETRACT_ANGLE + 40;
 
