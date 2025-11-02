8330e2d27202ee411ab15894efb6dd94bb20ce51
Vinh Nguyen <ngtv0404@gmail.com>
2025-11-02 15:52:01 +1100

    update lever angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
@@ -35,7 +35,7 @@ public class SorterSubsystem extends SubsystemBase {
     static final double C0_LEVER_ANGLE = C2_LEVER_ANGLE - 120;
     static final double C1_LEVER_ANGLE = C2_LEVER_ANGLE + 120;
     static final double LEVER_RETRACT_ANGLE = 25;
-    static final double LEVER_EXTEND_ANGLE = LEVER_RETRACT_ANGLE + 40;
+    static final double LEVER_EXTEND_ANGLE = LEVER_RETRACT_ANGLE + 80;
     private int currentCompartment = 0;
     private boolean toIntake = true;
     static Colour[] occupancy = new Colour[]{Colour.NONE, Colour.NONE, Colour.NONE};
