638adaf362b761c009c4d160f469f5a190f2715d
Vinh Nguyen <ngtv0404@gmail.com>
2025-11-06 14:26:24 +1100

    use servo subsystem for coordinated rotation (hopefully; untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SorterOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SorterOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SelectCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -21,29 +22,35 @@ public class SorterOpMode extends CommandOpMode {
         m_gamepad = new GamepadEx(gamepad1);
         m_sorterSubsystem = new SorterSubsystem(hardwareMap);
         m_gamepad.getGamepadButton(GamepadKeys.Button.LEFT_STICK_BUTTON)
-                .whenPressed(new InstantCommand(() -> {
-                    m_toIntake = !m_toIntake;
-                    m_sorterSubsystem.setSorterAngle(m_position, m_toIntake);
-                    telemetry.addData("Intake", m_toIntake);
-                    telemetry.addData("Position", m_position);
-                    telemetry.update();
-                }));
+                .whenPressed(
+                        new InstantCommand(() -> {
+                            m_toIntake = !m_toIntake;
+                            telemetry.addData("Intake", m_toIntake);
+                            telemetry.addData("Position", m_position);
+                            telemetry.update();
+                        })
+                                .andThen(new SelectCommand(() -> m_sorterSubsystem.setSorterAngleCommand(m_position, m_toIntake)))
+                );
         m_gamepad.getGamepadButton(GamepadKeys.Button.B)
-                .whenPressed(new InstantCommand(() -> {
-                    m_position++;
-                    if (m_position > 2)
-                        m_position = 0;
-                    m_sorterSubsystem.setSorterAngle(m_position, m_toIntake);
-                    telemetry.addData("Intake", m_toIntake);
-                    telemetry.addData("Position", m_position);
-                    telemetry.update();
-                }));
+                .whenPressed(
+                        new InstantCommand(() -> {
+                            m_position++;
+                            if (m_position > 2)
+                                m_position = 0;
+                            telemetry.addData("Intake", m_toIntake);
+                            telemetry.addData("Position", m_position);
+                            telemetry.update();
+                        })
+                                .andThen(new SelectCommand(() -> m_sorterSubsystem.setSorterAngleCommand(m_position, m_toIntake)))
+                );
         m_gamepad.getGamepadButton(GamepadKeys.Button.X)
-                .whenPressed(new InstantCommand(() -> {
-                    m_retract = !m_retract;
-                    m_sorterSubsystem.setLeverAngle(m_retract);
-                    telemetry.addData("Retract", m_retract);
-                    telemetry.update();
-                }));
+                .whenPressed(
+                        new InstantCommand(() -> {
+                            m_retract = !m_retract;
+                            telemetry.addData("Retract", m_retract);
+                            telemetry.update();
+                        })
+                                .andThen(new SelectCommand(() -> m_sorterSubsystem.setLeverAngleCommand(m_retract)))
+                );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SorterSubsystem.java
@@ -7,8 +7,6 @@ import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SelectCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.command.WaitCommand;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
@@ -18,8 +16,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class SorterSubsystem extends SubsystemBase {
     HardwareMap m_hardwareMap;
-    private SimpleServo m_leverServo;
-    private SimpleServo m_sorterServo;
+    private ServoSubsystem m_leverServo;
+    private ServoSubsystem m_sorterServo;
     static final double MIN_ANGLE = 0;
     static final double MAX_ANGLE = 300;
 //    static final double C2_LEVER_ANGLE = 180;
@@ -40,97 +38,54 @@ public class SorterSubsystem extends SubsystemBase {
     private boolean toIntake = true;
     public Colour[] occupancy = new Colour[]{Colour.NONE, Colour.NONE, Colour.NONE};
 
-
-    private static final double SORTER_WAIT_TIME = 400; // in ms
-    private static final double LEVER_WAIT_TIME = 300; // in ms
     private static final double SENSOR_WAIT_TIME = 200; // in ms
     private NormalizedColorSensor m_colourSensor;
 
     private static float COLOUR_SENSOR_GAIN = 21;
 
+    private static double LEVER_SERVO_SPEED = 115; // GoBilda Dual Mode Speed servo no-load speed @ 6.0V
+    private static double SORTER_SERVO_SPEED = 50; // GoBilda Dual Mode Torque servo no-load speed @ 6.0V
+    // NOTE: both servos are now powered by the REV Servo Hub
+
     public SorterSubsystem(HardwareMap hardwareMap) {
-        m_leverServo = new SimpleServo(hardwareMap, "leverServo", MIN_ANGLE, MAX_ANGLE);
-        m_sorterServo = new SimpleServo(hardwareMap, "sorterServo", MIN_ANGLE, MAX_ANGLE);
+        m_leverServo = new ServoSubsystem(hardwareMap, "leverServo", LEVER_SERVO_SPEED, MIN_ANGLE, MAX_ANGLE);
+        m_sorterServo = new ServoSubsystem(hardwareMap, "sorterServo", SORTER_SERVO_SPEED, MIN_ANGLE, MAX_ANGLE);
         m_colourSensor = hardwareMap.get(NormalizedColorSensor.class, "sorterColour");
         m_colourSensor.setGain(COLOUR_SENSOR_GAIN);
         m_hardwareMap = hardwareMap;
     }
-
-    public void setSorterAngle(int position, boolean toIntake){
-        setLeverAngle(true);
-        currentCompartment = position;
-        this.toIntake = toIntake;
-
-
-        /*
-            position:
-              0 - Compartment 1
-              1 - Compartment 2
-              2 - Compartment 3
-            toIntake: If true, then turning Compartment towards Intake, otherwise turning towards Lever
-         */
-        switch (position) {
-            case 0:
-                m_sorterServo.turnToAngle((toIntake ? C0_INTAKE_ANGLE : C0_LEVER_ANGLE));
-                break;
-            case 1:
-                m_sorterServo.turnToAngle((toIntake ? C1_INTAKE_ANGLE: C1_LEVER_ANGLE));
-                break;
-            case 2:
-                m_sorterServo.turnToAngle((toIntake ? C2_INTAKE_ANGLE : C2_LEVER_ANGLE));
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void feedUnoccupiedCompartment() {
-        for (int i = 0; i < 3; i++) {
-            if (occupancy[i] == Colour.NONE) {
-                setSorterAngle(i, true);
-                return;
-            }
-        }
-    }
-
     public Command feedUnoccupiedCompartmentCommand() {
-        return new InstantCommand(this::feedUnoccupiedCompartment, this)
-                .andThen(new WaitCommand((long) SORTER_WAIT_TIME));
-    }
-
-    public void loadIntoShooter(int position) {
-        setSorterAngle(position, false);
-        occupancy[position] = Colour.NONE;
-    }
-
-    public void loadIntoShooter(Colour colour) {
-        for (int pos = 0; pos < 3; pos++) {
-            if (occupancy[pos] == colour) {
-                loadIntoShooter(pos);
-                return;
+        return new SelectCommand(() -> {
+            for (int i = 0; i < 3; i++) {
+                if (occupancy[i] == Colour.NONE) {
+                    return setSorterAngleCommand(i, true);
+                }
             }
-        }
+            return new InstantCommand(() -> {}); // no-op
+        });
     }
 
     public Command loadIntoShooterCommand(int position) {
-        return new InstantCommand(() -> {
-            loadIntoShooter(position);
-        }, this)
-                .andThen(new WaitCommand((long) SORTER_WAIT_TIME))
-                .andThen(new InstantCommand(() -> { setLeverAngle(false); }, this))
-                .andThen(new WaitCommand((long) LEVER_WAIT_TIME))
-                .andThen(new InstantCommand(() -> {setLeverAngle(true);}, this));
+        return setSorterAngleCommand(position, false)
+                .andThen(
+                        setLeverAngleCommand(false)
+                                .alongWith(new InstantCommand(() -> {
+                                    occupancy[position] = Colour.NONE;
+                                }))
+                )
+                .andThen(setLeverAngleCommand(true));
     }
 
 
     public Command loadIntoShooterCommand(Colour colour) {
-        return new InstantCommand(() -> {
-            loadIntoShooter(colour);
-        }, this)
-                .andThen(new WaitCommand((long) SORTER_WAIT_TIME))
-                .andThen(new InstantCommand(() -> { setLeverAngle(false); }, this))
-                .andThen(new WaitCommand((long) LEVER_WAIT_TIME))
-                .andThen(new InstantCommand(() -> {setLeverAngle(true);}, this));
+        return new SelectCommand(() -> {
+            for (int pos = 0; pos < 3; pos++) {
+                if (occupancy[pos] == colour) {
+                    return loadIntoShooterCommand(pos);
+                }
+            }
+            return new InstantCommand(() -> {}); // no-op
+        });
     }
 
     public Command getColourCommand(){
@@ -138,9 +93,9 @@ public class SorterSubsystem extends SubsystemBase {
         AtomicInteger numGreen = new AtomicInteger();
         return new SelectCommand(
                 new HashMap<Object, Command>(){{
-                    put(0, setAngleCommand(2, false));
-                    put(1, setAngleCommand(0, false));
-                    put(2, setAngleCommand(1, false));
+                    put(0, setSorterAngleCommand(2, false));
+                    put(1, setSorterAngleCommand(0, false));
+                    put(2, setSorterAngleCommand(1, false));
 
 
                 }}, () -> {return currentCompartment;}
@@ -186,15 +141,36 @@ public class SorterSubsystem extends SubsystemBase {
 
                 ));
     }
-    public Command setAngleCommand(int position, boolean toIntake){
-        return new InstantCommand(() -> {
-            setSorterAngle(position, toIntake);
-        }, this)
-                .andThen(new WaitCommand((long) SORTER_WAIT_TIME));
+    public Command setSorterAngleCommand(int position, boolean toIntake){
+        return setLeverAngleCommand(true) // TODO: maybe we want to start moving before it pops all the way down?
+                .andThen(new SelectCommand(() -> {
+                    /*
+                        position:
+                          0 - Compartment 1
+                          1 - Compartment 2
+                          2 - Compartment 3
+                        toIntake: If true, then turning Compartment towards Intake, otherwise turning towards Lever
+                     */
+                    switch (position) {
+                        case 0:
+                            return m_sorterServo.setAngleCommand((toIntake ? C0_INTAKE_ANGLE : C0_LEVER_ANGLE));
+                        case 1:
+                            return m_sorterServo.setAngleCommand((toIntake ? C1_INTAKE_ANGLE: C1_LEVER_ANGLE));
+                        case 2:
+                            return m_sorterServo.setAngleCommand((toIntake ? C2_INTAKE_ANGLE : C2_LEVER_ANGLE));
+                        default:
+                            return new InstantCommand(() -> {}); // no-op
+                    }
+                }))
+                .andThen(new InstantCommand(() -> {
+                    currentCompartment = position;
+                    this.toIntake = toIntake;
+                }));
     }
-    public void setLeverAngle(boolean retract)
+
+    public Command setLeverAngleCommand(boolean retract)
     {
-        m_leverServo.turnToAngle((retract ? LEVER_RETRACT_ANGLE : LEVER_EXTEND_ANGLE));
+        return m_leverServo.setAngleCommand((retract ? LEVER_RETRACT_ANGLE : LEVER_EXTEND_ANGLE));
     }
 
     public enum Colour {
