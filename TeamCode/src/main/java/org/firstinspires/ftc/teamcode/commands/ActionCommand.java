04db6833b831f14f31645660e5464f93e07d872e
Vinh Nguyen <ngtv0404@gmail.com>
2025-11-16 07:31:13 +1100

    (WIP) fix ActionCommand stutter... somewhat?
    so far this is really only good for leaving and nothing else, but it's better than without the fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ActionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ActionCommand.java
@@ -5,6 +5,9 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.Subsystem;
+import com.arcrobotics.ftclib.util.Timing;
+
+import java.util.concurrent.TimeUnit;
 
 public class ActionCommand extends CommandBase {
     private final Action action;
@@ -20,6 +23,10 @@ public class ActionCommand extends CommandBase {
         action.preview(packet.fieldOverlay());
         finished = !action.run(packet);
         FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
+        Timing.Timer timer = new Timing.Timer(50, TimeUnit.MILLISECONDS);
+        timer.start();
+        while (!timer.done());
     }
 
     @Override
